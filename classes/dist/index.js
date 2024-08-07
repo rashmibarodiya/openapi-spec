4c843f3d43e6718d07de5e5c6f717418a089e64e
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-08-07 12:05:11 +0000

    complete classes

--- classes/dist/index.js
+++ classes/dist/index.js
@@ -1,4 +1,15 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const helpful_decorators_1 = require("helpful-decorators");
 class DateClass {
     constructor(place) {
         this.place = place;
@@ -8,6 +19,14 @@ class DateClass {
         return new Date().getTime();
     }
 }
-var d = new DateClass("India");
+__decorate([
+    helpful_decorators_1.measure,
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], DateClass.prototype, "getTime", null);
+const d = new DateClass("India");
 console.log(d.getTime());
-d.getTime();
+console.log("hi " + d.getTime());
+console.log(d.getTime());
+console.log(d.getTime());
